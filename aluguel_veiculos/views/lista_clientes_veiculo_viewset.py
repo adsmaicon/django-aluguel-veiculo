9ea4e16b02d2ef6232cfa376cae0572cd7d0d1da
Maicon Francisco de Carvalho <CIANDT\maiconfc@lnb035797cwb.ciandt.global>
2022-07-12 17:53:11 -0300

    First commit

---
+++ aluguel_veiculos/views/lista_clientes_veiculo_viewset.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from aluguel_veiculos.models.aluguel import Aluguel
+from aluguel_veiculos.serializers.lista_alugueis_cliente_serializer import ListaAlugueisClienteSerializer
+
+
+class ListaClientesVeiculoViewset(generics.ListAPIView):
+    """Listando as matrículas de um aluno ou aluna"""
+
+    def get_queryset(self):
+        queryset = Aluguel.objects.filter(veiculo_id=self.kwargs['pk'])
+        return queryset
+
+    serializer_class = ListaAlugueisClienteSerializer
